4084559b5613d1c8961057a85a51387e60cfb4dd
Muxen2 <max.anerud@gmail.com>
2026-04-24 13:35:29 +0200

    ss

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-document.addEventListener("click", function(event) {
-  if (event.target.tagName === "BUTTON") {
-    var link = event.target.querySelector("a");
-    if (link) {
-      window.location.href = store.html(link.getAttribute("href"));
+document.addEventListener("click", function(event) ) {
